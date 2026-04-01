e3f790d747b02d57bd7b8742c053202fee31f774
magda-tbf <magda@twobrothersfreight.com>
2026-04-01 17:46:17 +0200

    Delete back/backend directory

--- back/backend/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authRoutes = require('./routes/authRoutes');
-
-router.use('/api', authRoutes); // sve auth rute idu pod /api
-
-module.exports = router; // 🚨 exportujemo router, ne app
